4e7705998ed922240bffec157af0901198d9c95f
Laurent Brodeau <brodeau@gmail.com>
2017-09-25 17:53:04 +0200

    LB: adding mesh_mask light for ORCA12...

--- examples/Photo_test/conv_result_to_bmp.sh
+++ examples/Photo_test/conv_result_to_bmp.sh
@@ -2,6 +2,15 @@
 
 BRKD_DIR="${HOME}/DEV/barakuda"
 
-python ${BRKD_DIR}/python/exec/netcdf_to_image_bw.py bw_1600x900-1degx1deg_bilin.nc4 bw bmp
-python ${BRKD_DIR}/python/exec/netcdf_to_image_bw.py bw_1600x900-1degx1deg_smooth+bilin.nc4 bw bmp
 
+for ff in \
+        bw_400x225-0.25degx0.25deg_akima \
+        bw_400x225-0.25degx0.25deg_bilin \
+        bw_1600x900-1degx1deg_akima \
+        bw_1600x900-1degx1deg_smooth+akima \
+        bw_1600x900-1degx1deg_bilin \
+        bw_1600x900-1degx1deg_smooth+bilin; do
+
+    python ${BRKD_DIR}/python/exec/netcdf_to_image_bw.py ${ff}.nc4 bw bmp
+
+done
