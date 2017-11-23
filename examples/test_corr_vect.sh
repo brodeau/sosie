ab2c66967931d01ba58e8d7954221da8eb0a3dbf
Laurent Brodeau <brodeau@gmail.com>
2017-11-23 18:12:04 +0100

    Improved "corr_vect.f90"

--- examples/test_corr_vect.sh
+++ examples/test_corr_vect.sh
@@ -4,13 +4,10 @@
 ../bin/sosie.x -f namelist.example4_y
 
 
-# On T grid:
-../bin/corr_vect.x -x u10 -y v10 -G T -f namelist.example4_x -m data/mesh_mask_ORCA1_v3.6_light.nc4
-
 # On U,V grids:
-#../bin/corr_vect.x -x u10 -y v10 -G U -f namelist.example4_x -m data/mesh_mask_ORCA1_v3.6_light.nc4
+../bin/corr_vect.x -G U -f namelist.example4 -m data/mesh_mask_ORCA1v2_light.nc4
 
 
 #rm -f uraw*.nc* vraw*.nc*
 
-echo "Check files u10_1x1-deg-ORCA1_vct.nc4 and v10_1x1-deg-ORCA1_vct.nc4 !"; echo
+
