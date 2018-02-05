0944983e4d275b26e6b40a63611a10d34e80dae7
brodeau <brodeau@gmail.com>
2018-02-05 11:14:42 +0100

    LB

--- examples/ex_interp_to_ground_track/do_nachos12.sh
+++ examples/ex_interp_to_ground_track/do_nachos12.sh
@@ -2,9 +2,9 @@
 
 F_E="../data/dt_global_alg_sla_vxxc_20170402_SARAL-Altika.nc4"
 
-F_M="/home/laurent/DATA/SOSIE/NACHOS12.L75-MAA13_y2017m04d02.1h_SSH.nc4"
+F_M="${HOME}/DATA/SOSIE/NACHOS12.L75-MAA13_y2017m04d02.1h_SSH.nc4"
 
 ../../bin/interp_to_ground_track.x \
     -i ${F_M} -v sossheig \
-    -m /home/laurent/DATA/SOSIE/mesh_mask_1st_level_NACHOS12.nc4 \
-    -p ${F_E} -n sla_unfiltered
+    -m ${HOME}/DATA/SOSIE/mesh_mask_1st_level_NACHOS12.nc4 \
+    -p ${F_E} -n adt_unfiltered
