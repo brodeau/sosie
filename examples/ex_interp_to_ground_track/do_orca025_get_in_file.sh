2530dede94a881b2362fc29f5b9ae11ba89c3e10
brodeau <brodeau@gmail.com>
2018-02-12 12:46:02 +0100

    LB: various improvements...

--- examples/ex_interp_to_ground_track/do_orca025_get_in_file.sh
+++ examples/ex_interp_to_ground_track/do_orca025_get_in_file.sh
@@ -5,7 +5,7 @@ F_E="../data/dt_global_alg_sla_vxxc_20170402_SARAL-Altika.nc4"
 
 #F_E="../dt_short.nc"
 
-F_M="../data/ssh_ORCA025_2017.nc4"
+F_M="../data/ssh_ORCA025.nc4"
 
 ../../bin/interp_to_ground_track.x \
     -i ${F_M} -v ssh -x 'nav_lon' -y 'nav_lat' \
