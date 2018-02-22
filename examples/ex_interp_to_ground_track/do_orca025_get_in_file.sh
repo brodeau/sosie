bb471578df505f602a0315b1bf4e1f5c1aca62b8
brodeau <brodeau@gmail.com>
2018-02-22 13:43:18 +0100

    LB

--- examples/ex_interp_to_ground_track/do_orca025_get_in_file.sh
+++ examples/ex_interp_to_ground_track/do_orca025_get_in_file.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-F_E="../data/dt_global_alg_sla_vxxc_20170402_SARAL-Altika.nc4"
-#F_E="${HOME}/DATA/SOSIE/dt_global_s3a_sla_vxxc_20170402_Sentinel3.nc4"
+# we read everything in the model file, no mesh-mask provided!
 
-#F_E="../dt_short.nc"
+F_E="../data/dt_global_alg_sla_vxxc_20170402_SARAL-Altika.nc4"
 
 F_M="../data/ssh_ORCA025.nc4"
 
 ../../bin/interp_to_ground_track.x \
     -i ${F_M} -v ssh -x 'nav_lon' -y 'nav_lat' \
-    -p ${F_E} -n adt_unfiltered
-
-
-#    -p ${F_E} -n sla_unfiltered
-#    -m ../data/mesh_mask_ORCA1v2_light.nc4 \
+    -p ${F_E} -n adt_unfiltered -S
