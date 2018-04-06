02c2fabc6ce9eed9e7d11c4cfcbdddfc1ba01000
brodeau <brodeau@gmail.com>
2018-04-06 12:32:20 +0200

    LB

--- src/clean_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git filter-branch --index-filter 'git rm --cached --ignore-unmatch *.gz' -- --all
-rm -Rf .git/refs/original
-rm -Rf .git/logs/
-git gc --aggressive --prune=now
-git count-objects -v
