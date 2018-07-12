8277ff2e484d5308c6470fb8e9801f080ba148e2
brodeau <brodeau@gmail.com>
2018-07-12 23:19:33 +0200

    LB

---
+++ test_coarsening.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FMM="/MEDIA/data/eNATL4/eNATL4-I/mesh_mask_eNATL4_3.6.nc"
+FIN="/MEDIA/data/eNATL4/sossheig_snap_eNATL4.nc"
+
+for cv in "sossheig" "sosaline" "sosstsst"; do
+
+    ./bin/nemo_coarsener_2d.x -m ${FMM} -i ${FIN} -v ${cv} -o ${cv}_test.nc
+
+done
